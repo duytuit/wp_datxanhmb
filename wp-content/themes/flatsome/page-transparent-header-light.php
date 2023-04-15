6a8fc131fea9a1723cf3f51d0b5768f12624237f
Duy Tu <duytu89@gmail.com>
2023-04-15 09:15:24 +0700

    dev

--- wp-content/themes/flatsome/page-transparent-header-light.php
+++ wp-content/themes/flatsome/page-transparent-header-light.php
@@ -34,19 +34,19 @@ get_header(); ?>
 	}
 	?>
 	<!--Start of Tawk.to Script-->
-	<script type="text/javascript">
-		var Tawk_API = Tawk_API || {},
-			Tawk_LoadStart = new Date();
-		(function() {
-			var s1 = document.createElement("script"),
-				s0 = document.getElementsByTagName("script")[0];
-			s1.async = true;
-			s1.src = 'https://embed.tawk.to/640995d94247f20fefe4db82/1gr2n2n6o';
-			s1.charset = 'UTF-8';
-			s1.setAttribute('crossorigin', '*');
-			s0.parentNode.insertBefore(s1, s0);
-		})();
-	</script>
+<!--	<script type="text/javascript">-->
+<!--		var Tawk_API = Tawk_API || {},-->
+<!--			Tawk_LoadStart = new Date();-->
+<!--		(function() {-->
+<!--			var s1 = document.createElement("script"),-->
+<!--				s0 = document.getElementsByTagName("script")[0];-->
+<!--			s1.async = true;-->
+<!--			s1.src = 'https://embed.tawk.to/640995d94247f20fefe4db82/1gr2n2n6o';-->
+<!--			s1.charset = 'UTF-8';-->
+<!--			s1.setAttribute('crossorigin', '*');-->
+<!--			s0.parentNode.insertBefore(s1, s0);-->
+<!--		})();-->
+<!--	</script>-->
 	<!--End of Tawk.to Script-->
 </div>
 
